2eddd4ed4111cb159c7fe8c7a23e9bf23c5f1cd2
Jordan Harband <ljharb@gmail.com>
2015-10-12 23:46:35 -0700

    [Dev Deps] update `tape`, `jscs`, `eslint`, `@ljharb/eslint-config`

--- test/shimmed.js
+++ test/shimmed.js
@@ -9,6 +9,8 @@ var bind = require('function-bind');
 var isEnumerable = Object.prototype.propertyIsEnumerable;
 var functionsHaveNames = function f() {}.name === 'f';
 
+var runTests = require('./tests');
+
 test('shimmed', function (t) {
 	t.equal(String.prototype.padLeft.length, 1, 'String#padLeft has a length of 1');
 	t.test('Function name', { skip: !functionsHaveNames }, function (st) {
@@ -29,7 +31,7 @@ test('shimmed', function (t) {
 		st.end();
 	});
 
-	require('./tests')(bind.call(Function.call, String.prototype.padLeft), t);
+	runTests(bind.call(Function.call, String.prototype.padLeft), t);
 
 	t.end();
 });
