feb414f67e4e06718870ee1ac9f316df81eef2f5
Jordan Harband <ljharb@gmail.com>
2016-05-30 23:25:41 -0700

    [Dev Deps] update `jscs`, `nsp`, `eslint`, `@ljharb/eslint-config`

--- shim.js
+++ shim.js
@@ -5,6 +5,10 @@ var define = require('define-properties');
 
 module.exports = function shimPadStart() {
 	var polyfill = getPolyfill();
-	define(String.prototype, { padStart: polyfill }, { padStart: function () { return String.prototype.padStart !== polyfill; } });
+	define(String.prototype, { padStart: polyfill }, {
+		padStart: function testPadStart() {
+			return String.prototype.padStart !== polyfill;
+		}
+	});
 	return polyfill;
 };
