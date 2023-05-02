c34532c8d1b459aa7037e752be350afaa20f7bc5
Jordan Harband <ljharb@gmail.com>
2023-05-02 11:07:38 -0700

    [meta] update dev workflows

--- shim.js
+++ shim.js
@@ -5,6 +5,12 @@ var define = require('define-properties');
 
 module.exports = function shimPadLeft() {
 	var polyfill = getPolyfill();
-	define(String.prototype, { padLeft: polyfill }, { padLeft: function () { return String.prototype.padLeft !== polyfill; } });
+
+	define(
+		String.prototype,
+		{ padLeft: polyfill },
+		{ padLeft: function () { return String.prototype.padLeft !== polyfill; } }
+	);
+
 	return polyfill;
 };
--- test/index.js
+++ test/index.js
@@ -6,8 +6,8 @@ var runTests = require('./tests');
 
 test('as a function', function (t) {
 	t.test('bad array/this value', function (st) {
-		st.throws(function () { padLeft(undefined, 'a'); }, TypeError, 'undefined is not an object');
-		st.throws(function () { padLeft(null, 'a'); }, TypeError, 'null is not an object');
+		st['throws'](function () { padLeft(undefined, 'a'); }, TypeError, 'undefined is not an object');
+		st['throws'](function () { padLeft(null, 'a'); }, TypeError, 'null is not an object');
 		st.end();
 	});
 
